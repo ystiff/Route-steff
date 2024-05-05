df52a94ec45a540ec5aa7ff403299ccccfa09359
ystiff <114044523+ystiff@users.noreply.github.com>
2024-05-05 23:23:02 +0800

    case-study

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,5 @@
 <?php
-use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,8 +13,3 @@ use Illuminate\Support\Facades\Route;
 
 
  
-Route::get('/greetings', [UserController::class,'index']);
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
